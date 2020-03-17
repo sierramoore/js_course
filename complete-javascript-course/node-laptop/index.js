e7b16a25731fa3383df9b54fa9a6b582e02a4690
Sierra Moore <sierra.v.moore@gmail.com>
2020-03-17 23:59:20 +0100

    outputed json data to server

--- complete-javascript-course/node-laptop/index.js
+++ complete-javascript-course/node-laptop/index.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'); // okay to be blocking bc only happens once on start
 const laptopData = JSON.parse(json);
 
+// each time someone access it this will excute
 const server = http.createServer((req, res) => {
 
     const pathName = url.parse(req.url, true).pathname;
@@ -15,10 +16,33 @@ const server = http.createServer((req, res) => {
     if(pathName === '/products' || pathName === '/') {
         res.writeHead(200, {'Content-type': 'text/html'});
         res.end(`this is the products page`);
+
     } else if(pathName === '/laptop' && id < laptopData.length) { // and exists
+
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(`this is the laptop page for ${id}`);
+
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+
+            const laptop = laptopData[id];
+            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+            // replace quotes with //g to make regular expression
+            output = output.replace(/{%IMG%}/g, laptop.image);
+            output = output.replace(/{%IMG%}/g, laptop.image);
+            output = output.replace(/{%PRICE%}/g, laptop.price);
+            output = output.replace(/{%SCREEN%}/g, laptop.screen);
+            output = output.replace(/{%CPU%}/g, laptop.cpu);
+            output = output.replace(/{%STORAGE%}/g, laptop.storage);
+            output = output.replace(/{%RAM%}/g, laptop.ram);
+            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+
+            //send response back
+            res.end(output);
+
+        }); //sync vs will block code while executing
+        // have access to data bc readFile will fetch data and in callback have access to data
+
     } else {
+
         res.writeHead(404, {'Content-type': 'text/html'});
         res.end('url wasnt found on server');
     }
